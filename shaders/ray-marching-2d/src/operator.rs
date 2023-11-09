1fa70f94c923686cca3ea8d3a7033d60f35dabba
Abel Svoboda <abel465@gmail.com>
2023-11-10 10:25:39 +1300

    Fix repeat operators

--- shaders/ray-marching-2d/src/operator.rs
+++ shaders/ray-marching-2d/src/operator.rs
@@ -2,15 +2,15 @@ use spirv_std::glam::{vec2, Vec2};
 use spirv_std::num_traits::Euclid;
 
 pub fn repeat_x(p: Vec2, factor: f32) -> Vec2 {
-    let x = Euclid::rem_euclid(&p.x, &factor) - 0.5 * factor;
+    let x = Euclid::rem_euclid(&(p.x - 0.5 * factor), &factor) - 0.5 * factor;
     vec2(x, p.y)
 }
 
 pub fn repeat_y(p: Vec2, factor: f32) -> Vec2 {
-    let y = Euclid::rem_euclid(&p.y, &factor) - 0.5 * factor;
+    let y = Euclid::rem_euclid(&(p.y - 0.5 * factor), &factor) - 0.5 * factor;
     vec2(p.x, y)
 }
 
 pub fn repeat_xy(p: Vec2, factor: Vec2) -> Vec2 {
-    p.rem_euclid(factor) - 0.5 * factor
+    (p - 0.5 * factor).rem_euclid(factor) - 0.5 * factor
 }
