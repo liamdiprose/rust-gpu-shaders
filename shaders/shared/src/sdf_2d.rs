361046bc81010865352db83612aa5c4f61cb1774
Abel Svoboda <abel465@gmail.com>
2023-11-15 21:09:35 +1300

    Add 2d sdf's

--- shaders/shared/src/sdf_2d.rs
+++ shaders/shared/src/sdf_2d.rs
@@ -1,10 +1,6 @@
 use spirv_std::glam::{vec2, Vec2};
 use spirv_std::num_traits::Float;
 
-pub fn plane(p: Vec2) -> f32 {
-    p.y
-}
-
 pub fn circle(p: Vec2, r: f32) -> f32 {
     p.length() - r
 }
@@ -16,7 +12,30 @@ pub fn rectangle(p: Vec2, dim: Vec2) -> f32 {
     e + i
 }
 
-pub fn line(p: Vec2, a: Vec2, b: Vec2) -> f32 {
+/// d must be normalized or else it will scale space
+pub fn infinite_plane(p: Vec2, d: Vec2) -> f32 {
+    d.perp_dot(p)
+}
+
+/// d must be normalized or else it will scale space
+pub fn infinite_line(p: Vec2, d: Vec2) -> f32 {
+    infinite_plane(p, d).abs()
+}
+
+pub fn ray(p: Vec2, d: Vec2) -> f32 {
+    let t = (p.dot(d) / d.length_squared()).max(0.0);
+    p.distance(t * d)
+}
+
+pub fn plane_ray(p: Vec2, d: Vec2) -> f32 {
+    ray(p, d) * infinite_plane(p, d).signum()
+}
+
+pub fn plane_segment(p: Vec2, a: Vec2, b: Vec2) -> f32 {
+    line_segment(p, a, b) * infinite_plane(p - a, b - a).signum()
+}
+
+pub fn line_segment(p: Vec2, a: Vec2, b: Vec2) -> f32 {
     let ap = p - a;
     let ab = b - a;
     let t = (ap.dot(ab) / ab.length_squared()).clamp(0.0, 1.0);
@@ -25,7 +44,7 @@ pub fn line(p: Vec2, a: Vec2, b: Vec2) -> f32 {
 }
 
 pub fn capsule(p: Vec2, a: Vec2, b: Vec2, r: f32) -> f32 {
-    line(p, a, b) - r
+    line_segment(p, a, b) - r
 }
 
 pub fn torus(p: Vec2, r: Vec2) -> f32 {
@@ -33,11 +52,11 @@ pub fn torus(p: Vec2, r: Vec2) -> f32 {
 }
 
 pub fn equilateral_triangle(mut p: Vec2, r: f32) -> f32 {
+    p.x = p.x.abs();
     let k = Float::sqrt(3.0);
-    p = vec2(Float::abs(p.x) - r, p.y + r / k);
-    if p.x + k * p.y > 0.0 {
-        p = vec2(p.x - k * p.y, -k * p.x - p.y) / 2.0;
-    }
-    p.x -= p.x.clamp(-2.0 * r, 0.0);
-    -p.length() * Float::signum(p.y)
+
+    Float::max(
+        plane_ray(p - vec2(r, -r / k), Vec2::NEG_X),
+        plane_segment(p, vec2(0.0, 2.0 * r / k), vec2(r, -r / k)),
+    )
 }
