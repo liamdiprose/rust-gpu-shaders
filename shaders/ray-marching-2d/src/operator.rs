d52372dc1621d4f15f562f0cda49355053ce9bfd
Abel Svoboda <abel465@gmail.com>
2023-11-17 12:10:32 +1300

    Improve 2d sdf repeat functions

--- shaders/ray-marching-2d/src/operator.rs
+++ shaders/ray-marching-2d/src/operator.rs
@@ -1,16 +1,13 @@
 use spirv_std::glam::{vec2, Vec2};
-use spirv_std::num_traits::Euclid;
 
 pub fn repeat_x(p: Vec2, factor: f32) -> Vec2 {
-    let x = Euclid::rem_euclid(&(p.x - 0.5 * factor), &factor) - 0.5 * factor;
-    vec2(x, p.y)
+    p - factor * vec2(p.x / factor, p.y).round()
 }
 
 pub fn repeat_y(p: Vec2, factor: f32) -> Vec2 {
-    let y = Euclid::rem_euclid(&(p.y - 0.5 * factor), &factor) - 0.5 * factor;
-    vec2(p.x, y)
+    p - factor * vec2(p.x, p.y / factor).round()
 }
 
 pub fn repeat_xy(p: Vec2, factor: Vec2) -> Vec2 {
-    (p - 0.5 * factor).rem_euclid(factor) - 0.5 * factor
+    p - factor * vec2(p.x / factor.x, p.y / factor.y).round()
 }
