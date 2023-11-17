2a7c2de2ef7a61efceb2188e267628ec69eccc8e
Abel Svoboda <abel465@gmail.com>
2023-11-17 13:42:38 +1300

    Fix 2d sdf repeat ops

--- shaders/ray-marching-2d/src/operator.rs
+++ shaders/ray-marching-2d/src/operator.rs
@@ -1,13 +1,15 @@
 use spirv_std::glam::{vec2, Vec2};
+#[allow(unused_imports)]
+use spirv_std::num_traits::Float;
 
 pub fn repeat_x(p: Vec2, factor: f32) -> Vec2 {
-    p - factor * vec2(p.x / factor, p.y).round()
+    vec2(p.x - factor * (p.x / factor).round(), p.y)
 }
 
 pub fn repeat_y(p: Vec2, factor: f32) -> Vec2 {
-    p - factor * vec2(p.x, p.y / factor).round()
+    vec2(p.x, p.y - factor * (p.y / factor).round())
 }
 
-pub fn repeat_xy(p: Vec2, factor: Vec2) -> Vec2 {
+pub fn repeat_xy(mut p: Vec2, factor: Vec2) -> Vec2 {
     p - factor * vec2(p.x / factor.x, p.y / factor.y).round()
 }
