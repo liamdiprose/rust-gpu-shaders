53fd0e6f0fc5ea3bc964fe396f6bd66678522c40
Liam Diprose <liam@liamdiprose.com>
2023-12-19 16:27:33 +1300

    Correct WGPU_BACKEND

--- flake.nix
+++ flake.nix
@@ -26,9 +26,14 @@
       {
         devShell = with pkgs; mkShell rec {
 
-          nativeBuildInputs = [ pkg-config ];
+          nativeBuildInputs = [ 
+            pkg-config 
+            vulkan-tools
+          ];
           
-          LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
+          LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}:/run/opengl-driver/lib";
+
+          WGPU_BACKEND = "vulkan";
 
           buildInputs = [
             rustPkg
