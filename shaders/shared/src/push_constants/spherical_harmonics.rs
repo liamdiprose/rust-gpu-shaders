fc223009266678e5a3e5a01df67c2ac66ba71816
Abel Svoboda <abel465@gmail.com>
2024-02-19 11:20:29 +1300

    Add real variant to spherical harmonics

--- shaders/shared/src/push_constants/spherical_harmonics.rs
+++ shaders/shared/src/push_constants/spherical_harmonics.rs
@@ -1,6 +1,23 @@
 use crate::push_constants::{Quat, Size, Vec2};
 use bytemuck::{Pod, Zeroable};
 
+#[derive(PartialEq, Copy, Clone)]
+#[repr(u32)]
+pub enum Variant {
+    Real,
+    Complex,
+}
+
+impl Variant {
+    pub fn from_u32(x: u32) -> Self {
+        if x >= core::mem::variant_count::<Variant>() as u32 {
+            Variant::Real
+        } else {
+            unsafe { core::mem::transmute(x) }
+        }
+    }
+}
+
 #[derive(Copy, Clone, Pod, Zeroable)]
 #[repr(C)]
 pub struct ShaderConstants {
@@ -13,4 +30,5 @@ pub struct ShaderConstants {
     /// Bit mask of the pressed buttons (0 = Left, 1 = Middle, 2 = Right).
     pub mouse_button_pressed: u32,
     pub quat: Quat,
+    pub variant: u32,
 }
