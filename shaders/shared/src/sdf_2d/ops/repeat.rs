d6a39fec8f443d4b500f1fb5d6af384ff9524bde
Abel Svoboda <abel465@gmail.com>
2024-03-05 12:58:28 +1300

    Add rotational repetion to sdfs 2d

--- shaders/shared/src/sdf_2d/ops/repeat.rs
+++ shaders/shared/src/sdf_2d/ops/repeat.rs
@@ -1,4 +1,6 @@
-use spirv_std::glam::{vec2, Vec2};
+use crate::functional::tuple::*;
+use core::f32::consts::TAU;
+use spirv_std::glam::{vec2, Mat2, Vec2};
 #[cfg_attr(not(target_arch = "spirv"), allow(unused_imports))]
 use spirv_std::num_traits::Float;
 
@@ -6,7 +8,7 @@ use spirv_std::num_traits::Float;
 pub struct Repeat<const N: i32 = 0>;
 
 impl<const N: i32> Repeat<N> {
-    pub fn repeat_x<F>(p: Vec2, factor: f32, f: F) -> f32
+    pub fn repeat_x<F>(p: Vec2, factor: f32, sdf: F) -> f32
     where
         F: Fn(Vec2) -> f32,
     {
@@ -17,12 +19,12 @@ impl<const N: i32> Repeat<N> {
         for i in (0 - N)..(2 + N) {
             let rid = id + i as f32 * o;
             let r = vec2(p.x - factor * rid, p.y);
-            d = d.min(f(r));
+            d = d.min(sdf(r));
         }
         d
     }
 
-    pub fn repeat_y<F>(p: Vec2, factor: f32, f: F) -> f32
+    pub fn repeat_y<F>(p: Vec2, factor: f32, sdf: F) -> f32
     where
         F: Fn(Vec2) -> f32,
     {
@@ -33,12 +35,12 @@ impl<const N: i32> Repeat<N> {
         for i in (0 - N)..(2 + N) {
             let rid = id + i as f32 * o;
             let r = vec2(p.x, p.y - factor * rid);
-            d = d.min(f(r));
+            d = d.min(sdf(r));
         }
         d
     }
 
-    pub fn repeat_xy<F>(p: Vec2, factor: Vec2, f: F) -> f32
+    pub fn repeat_xy<F>(p: Vec2, factor: Vec2, sdf: F) -> f32
     where
         F: Fn(Vec2) -> f32,
     {
@@ -50,57 +52,24 @@ impl<const N: i32> Repeat<N> {
             for j in (0 - N)..(2 + N) {
                 let rid = id + vec2(i as f32, j as f32) * o;
                 let r = p - factor * rid;
-                d = d.min(f(r));
+                d = d.min(sdf(r));
             }
         }
         d
     }
 }
 
-// impl<const N: u32> Repeat<N> {
-//     pub fn repeat_x(p: Vec2, factor: f32) -> [Vec2; 2 + 2 * N as usize] {
-//         let id = (p.x / factor).round();
-//         let o = (p.x - factor * id).signum();
-//         let mut result = [Vec2::ZERO; 2 + 2 * N as usize];
-//
-//         for i in (0 - N as i32)..(2 + N as i32) {
-//             let rid = id + i as f32 * o;
-//             let r = vec2(p.x - factor * rid, p.y);
-//             result[i as usize] = r;
-//         }
-//
-//         result
-//     }
-//
-//     pub fn repeat_y(p: Vec2, factor: f32) -> [Vec2; 2 + 2 * N as usize] {
-//         let id = (p.y / factor).round();
-//         let o = (p.y - factor * id).signum();
-//         let mut result = [Vec2::ZERO; 2 + 2 * N as usize];
-//
-//         for i in (0 - N as i32)..(2 + N as i32) {
-//             let rid = id + i as f32 * o;
-//             let r = vec2(p.x, p.y - factor * rid);
-//             result[i as usize] = r;
-//         }
-//
-//         result
-//     }
-//
-//     pub fn repeat_xy<F>(p: Vec2, factor: Vec2, f: F) -> f32
-//     where
-//         F: Fn(Vec2) -> f32,
-//     {
-//         let id = (p / factor).round();
-//         let o = (p - factor * id).signum();
-//
-//         let mut d = 1e20;
-//         for i in (0 - N as i32)..(2 + N as i32) {
-//             for j in (0 - N as i32)..(2 + N as i32) {
-//                 let rid = id + vec2(i as f32, j as f32) * o;
-//                 let r = p - factor * rid;
-//                 d = d.min(f(r));
-//             }
-//         }
-//         d
-//     }
-// }
+/// Repeats `n` times around a circle of radius `r`
+pub fn repeat_r<F>(p: Vec2, n: i32, r: f32, sdf: F) -> f32
+where
+    F: Fn(Vec2) -> f32,
+{
+    let sp = TAU / n as f32;
+    let an = p.y.atan2(p.x);
+    let id = (an / sp).floor();
+    (-sp * id, -sp * (id + 1.0))
+        .map(Mat2::from_angle)
+        .map(|x| x * p - vec2(r, 0.0))
+        .map(sdf)
+        .min_element()
+}
