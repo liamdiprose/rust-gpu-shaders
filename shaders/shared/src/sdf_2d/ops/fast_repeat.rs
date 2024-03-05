f1a4785bfd31543af18b5f196e34c53c5c21d842
Abel Svoboda <abel465@gmail.com>
2024-03-05 14:07:28 +1300

    Add mirrored repetition to 2d sdfs

--- shaders/shared/src/sdf_2d/ops/fast_repeat.rs
+++ shaders/shared/src/sdf_2d/ops/fast_repeat.rs
@@ -3,16 +3,40 @@ use spirv_std::glam::{vec2, Vec2};
 use spirv_std::num_traits::Float;
 
 // the sdf must be symmetric with respect to the tile boundaries
-pub fn repeat_x(p: Vec2, factor: f32) -> Vec2 {
-    vec2(p.x - factor * (p.x / factor).round(), p.y)
+pub fn repeat_x(p: Vec2, s: f32) -> Vec2 {
+    vec2(p.x - s * (p.x / s).round(), p.y)
 }
 
 // the sdf must be symmetric with respect to the tile boundaries
-pub fn repeat_y(p: Vec2, factor: f32) -> Vec2 {
-    vec2(p.x, p.y - factor * (p.y / factor).round())
+pub fn repeat_y(p: Vec2, s: f32) -> Vec2 {
+    vec2(p.x, p.y - s * (p.y / s).round())
 }
 
 // the sdf must be symmetric with respect to the tile boundaries
-pub fn repeat_xy(p: Vec2, factor: Vec2) -> Vec2 {
-    p - factor * (p / factor).round()
+pub fn repeat_xy(p: Vec2, s: Vec2) -> Vec2 {
+    p - s * (p / s).round()
+}
+
+// fast space repetition by mirroring every other instance
+pub fn repeat_mirrored(p: Vec2, s: f32) -> Vec2 {
+    let id = (p / s).round();
+    let r = p - s * id;
+    vec2(
+        if (id.x as i32 & 1) == 0 { r.x } else { -r.x },
+        if (id.y as i32 & 1) == 0 { r.y } else { -r.y },
+    )
+}
+
+// fast space repetition by mirroring every other instance
+pub fn repeat_mirrored_x(p: Vec2, s: f32) -> Vec2 {
+    let id = (p.x / s).round();
+    let r = p.x - s * id;
+    vec2(if (id as i32 & 1) == 0 { r } else { -r }, p.y)
+}
+
+// fast space repetition by mirroring every other instance
+pub fn repeat_mirrored_y(p: Vec2, s: f32) -> Vec2 {
+    let id = (p.y / s).round();
+    let r = p.y - s * id;
+    vec2(p.x, if (id as i32 & 1) == 0 { r } else { -r })
 }
