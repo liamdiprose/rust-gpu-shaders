daa319e7b5c6bbdd7714daf302270e83d426bf26
Abel Svoboda <abel465@gmail.com>
2024-03-12 17:14:10 +1300

    Add first person camera

--- runner/src/camera.rs
+++ runner/src/camera.rs
@@ -1,6 +1,10 @@
 use core::f32::consts::PI;
-use glam::{Mat4, Quat, Vec2, Vec2Swizzles, Vec3};
-use winit::dpi::PhysicalSize;
+use glam::{vec3, Mat4, Quat, Vec2, Vec2Swizzles, Vec3};
+use std::time::Duration;
+use winit::{
+    dpi::PhysicalSize,
+    event::{ElementState, KeyboardInput},
+};
 
 #[derive(Clone)]
 pub struct RotationCamera {
@@ -11,7 +15,7 @@ pub struct RotationCamera {
 
 impl RotationCamera {
     pub fn new(aspect: f32, zoom: f32) -> Self {
-        RotationCamera {
+        Self {
             aspect,
             zoom,
             rot: Quat::IDENTITY,
@@ -36,5 +40,93 @@ impl RotationCamera {
 
     pub fn zoom(&mut self, zoom: f32) {
         self.zoom *= zoom;
+    }
+}
+
+#[derive(Clone)]
+pub struct FirstPersonCamera {
+    pub aspect: f32,
+    pub position: Vec3,
+    pub zoom: f32,
+    pub yaw: f32,
+    pub pitch: f32,
+    pub amount_forward: f32,
+    pub amount_backward: f32,
+    pub amount_left: f32,
+    pub amount_right: f32,
+}
+
+impl FirstPersonCamera {
+    pub fn new(aspect: f32, position: Vec3, zoom: f32) -> Self {
+        Self {
+            aspect,
+            position,
+            zoom,
+            yaw: -PI / 2.0,
+            pitch: PI,
+            amount_forward: 0.0,
+            amount_backward: 0.0,
+            amount_left: 0.0,
+            amount_right: 0.0,
+        }
+    }
+
+    pub fn rotate(&mut self, v: Vec2) {
+        self.pitch = (self.pitch - v.y * 2.0).clamp(-PI / 3.0, PI / 3.0);
+        self.yaw = self.yaw - v.x * 3.0;
+    }
+
+    pub fn resize(&mut self, size: PhysicalSize<u32>) {
+        self.aspect = size.width as f32 / size.height as f32;
+    }
+
+    pub fn zoom(&mut self, zoom: f32) {
+        self.zoom *= zoom;
+    }
+
+    pub fn update(&mut self, dt: Duration) {
+        let dt = dt.as_secs_f32();
+        let (yaw_sin, yaw_cos) = self.yaw.sin_cos();
+        let forward = vec3(-yaw_sin, 0.0, -yaw_cos).normalize();
+        let right = vec3(yaw_cos, 0.0, -yaw_sin).normalize();
+
+        let translate = forward * (self.amount_forward - self.amount_backward)
+            + right * (self.amount_right - self.amount_left);
+        self.position += translate.normalize_or_zero() * 4.0 * dt;
+    }
+
+    pub fn keyboard_input(&mut self, input: KeyboardInput) {
+        let pressed = input.state == ElementState::Pressed;
+        let amount = if pressed { 1.0 } else { 0.0 };
+        use winit::event::VirtualKeyCode;
+        if let Some(keycode) = input.virtual_keycode {
+            match keycode {
+                VirtualKeyCode::W => {
+                    self.amount_forward = amount;
+                    if pressed {
+                        self.amount_backward = 0.0;
+                    }
+                }
+                VirtualKeyCode::S => {
+                    self.amount_backward = amount;
+                    if pressed {
+                        self.amount_forward = 0.0;
+                    }
+                }
+                VirtualKeyCode::A => {
+                    self.amount_left = amount;
+                    if pressed {
+                        self.amount_right = 0.0;
+                    }
+                }
+                VirtualKeyCode::D => {
+                    self.amount_right = amount;
+                    if pressed {
+                        self.amount_left = 0.0;
+                    }
+                }
+                _ => {}
+            }
+        }
     }
 }
